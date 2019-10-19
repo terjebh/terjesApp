27c784cf0fa9348cc93fe17ef798855df1254f42
Aina Berg-Hansen <ainaberg-hansen@Ainas-MacBook-Air.local>
2019-10-19 18:21:33 +0200

    workd

--- model/result_company.js
+++ model/result_company.js
@@ -1,5 +1,5 @@
 const db = require('./db');
-let result_company = "";
+let result_company;
 
 // SQL
 const query_company= "select firma_id as ID, firmanavn as Firmanavn, epost as Epost, tel as Telefon from firma";
@@ -10,9 +10,9 @@ db.connection.query(query_company, async function (err, result, fields) {
     } else {
         // console.log("Firma:" + result[0].Firmanavn);
         result_company = await result;
+        module.exports.result_company = result_company;
     }
 });
 
 
 
-module.exports.result_company = result_company;
--- model/result_person.js
+++ model/result_person.js
@@ -1,16 +1,16 @@
 const db = require('./db');
-var result_person ="";
+var result_person;
 
 // SQL
 const query_person = "select person_id as ID, concat(fornavn,' ',etternavn) as Navn, epost as Epost, tel as Telefon from person";
-
+ 
 db.connection.query(query_person, async function (err, result, fields) {
     if (err) {
         throw err;
     } else {
-        // console.log("Personer:" + result[0].Navn);
         result_person = await result;
+        //console.log("Personer:" + result_person);
+        module.exports.result_person = result_person;
     }
 });
 
-module.exports.result_person = result_person;
